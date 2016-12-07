a7556babd305ee61f2e097b1f82620420f13ba58
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-12-07 11:22:25 -0800

    in-class app.js

--- sample-code-header-solution/js/app.js
+++ sample-code-header-solution/js/app.js
@@ -6,30 +6,38 @@ $(document).ready(function(){
   var source = $('#developer-li-template').html();
   // console.log('template script source:', source);
 
-  var headerSource = $('#header-template').html();
+  var headerSource = $('#header-info-template').html();
+  console.log('header template script source:', headerSource);
 
   // compile the handlebars template
   var template = Handlebars.compile(source);
+  // console.log(template);
 
+  var headerSource = $('#header-info-template').html();
   var headerTemplate = Handlebars.compile(headerSource);
+  var headerHtml = headerTemplate({
+    cohortNumber: data.cohort,
+    roomNumber: data.room,
+    scheduleUrl: data.github_schedule
+  });
+  console.log(headerHtml);
+
+  $('header').append(headerHtml);
 
   // use the template function from handlebars to create an HTML string
   // the template function takes in an object where:
     // each key is a variable the html template expects
     // each key's value is the data we want that variable to have
-  var developerHtml = template({ developers: data.developers });
-  // console.log('generated html string:', developerHtml);
+  data.developers.forEach(function(developer){
+    // this stuff is happening for each person
+    var developerHtml = template({
+      github_username: developer.github_username,
+      first_name: developer.first_name,
+      last_name: developer.last_name
+     });
+    // console.log('generated html string:', developerHtml);
 
-  var headerHtml = headerTemplate({
-    cohort: data.cohort,
-    classroom: data.room,
-    schedule_link: data.github_schedule
+    // append html to the view
+    $("#developers-list").append(developerHtml);
   });
-  console.log(headerHtml);
-
-  // append html to the view
-  $("#developers-list").append(developerHtml);
-
-  $('#cohort-header').append(headerHtml);
-
 });
